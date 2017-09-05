3e40ade24aebe743f1183945edd6183b07b2fc56
Florian Maxim <look@cheesyeyes.com>
2017-09-05 11:44:23 +0200

    added style support

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -20,6 +20,16 @@ export default {
         test: /\.(js)$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+          }, {
+              loader: "css-loader" // translates CSS into CommonJS
+          }, {
+              loader: "sass-loader" // compiles Sass to CSS
+        }]
       }
     ]
   },
